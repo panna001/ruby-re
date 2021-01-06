b736a1855c6df1f23da372cdce08e16c30fe6803
DaichiIshikawa <sonar.pocket.0926@gmail.com>
2021-01-06 03:52:43 +0000

    [Finish]2-8

---
+++ fizz_buzz.rb
@@ -0,0 +1,15 @@
+def fizz_buzz(number)
+  if (number % 3 == 0) && (number % 5 == 0) 
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
+end
+
+puts "数字を入力してください"
+input = gets.to_i
+puts fizz_buzz(input)
